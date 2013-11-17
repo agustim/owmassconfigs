77c4df4487e1e4606507769d1772babc1ae65ac8
Agustí Moll <agusti@gmail.com>
2013-11-17 09:21:10 +0100

    Generalitzant els sistema

--- make_all.sh
+++ make_all.sh
@@ -17,7 +17,7 @@ make_all()
 		if [ -f $KIND.qmp.save.tar.gz ];
 			then
 				echo Genera el ftixer $HN
-				./make_recoved_file.sh $IP $HN $CN
+				make_recoved_file $IP $HN $CN 
 				if [ ! -z $destination]
 				then
 					mv $KIND.qmp.save.tar.gz $destination
@@ -41,4 +41,55 @@ qmpdef_recover()
 make_recoved_file()
 {
 	
+FILEDATA=${4:-tlwr841v8.qmp.save.tar.gz}
+DIR=${5:-localtmp}
+
+if [ $# -lt 3 ] 
+	then
+	echo "Generate qmp recover status file. To use in node."
+	echo "	$0 <IP> <HOSTNAME> <CHANNEL> [filename. Default: ${FILEDATA}] [work directory. Default: ${DIR}]"
+	return
+fi
+if [ ! -f ${FILEDATA} ];
+then
+	echo "Doesn't exist ${FILEDATA}."
+	return
+fi
+
+HOSTNAMEDEST=$2
+
+mkdir -p $DIR
+cd $DIR
+
+tar zxf ../$FILEDATA 
+
+while read c
+do
+	ORIG=$(echo "$c" | cut -d "|" -f 1)
+	DEST=$(echo "$c" | cut -d "|" -f 2)
+
+	for i in $DEST
+	do 
+        P=$(echo $i|grep -o '\$[[:digit:]]\+')
+        if [ ! -z $P ]
+        then
+                eval PARM=$P
+                DEST=$(echo "$DEST"|sed -e "s/${P}/${PARM}/g")
+        fi
+	done
+	echo "Change '$ORIG' -> '$DEST'"
+
+	for i in $(fgrep -sR "$ORIG" *|cut -d: -f1|sort -u); 
+	do 
+		sed -i -e "s|$ORIG|$DEST|g" $i; 
+	done
+
+done < ../changes.txt
+
+tar zcf ../${HOSTNAMEDEST}.qmp.save.tar.gz *	
+cd ..
+rm -rf $DIR
+
+echo "The ${HOSTNAMEDEST}.qmp.save.tar.gz was create."
 }
+
